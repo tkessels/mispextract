773f14e4ceff423522d0173206cfcb9e5356491c
Tobias Kessels <tabledevil@gmail.com>
2017-08-05 23:57:18 +0200

    Added Argument Parsing
    Added Argument Parsing and added corresponding Features.
    + quiet - mode = no Output
    + file option to specify name of jason file
    + help option to display usage information
    + added interactive switch wo funtion TODO: implement filtering and tag browsing
    ~ rearranged code a bit

--- misp_extract.py
+++ misp_extract.py
@@ -1,7 +1,56 @@
 import json
 import re
 import sys
-misp_export='misp.json'
+import getopt
+
+
+class JsonProgress(object):
+    def __init__(self):
+        self.count = 0
+
+    def __call__(self, obj):
+        self.count += 1
+        if (self.count % 100)==0:
+            sys.stdout.write("\r%8d" % self.count)
+        return obj
+
+
+def print_usage(n):
+    print("Usage: %s [-h] [-i] [-q] [-f misp_export.json]"%sys.argv[0])
+    sys.exit(n)
+
+def print_stats():
+    global stats_to_clear
+    sys.stdout.write('\x1b[1A\x1b[2K'*stats_to_clear)
+    sys.stdout.write('\x1b[0G')
+
+    width=40
+    stats_to_clear=0
+    if progress["event"][1]>0:
+        progress_barlength=(progress["event"][0]*width)/progress["event"][1]
+        progress_percentage=(progress["event"][0]*100)/progress["event"][1]
+        print("[++] [{:_<{wdt}}] {:03d}%".format("+"*int(progress_barlength),int(progress_percentage),wdt=width))
+        stats_to_clear+=1
+
+    print("[++]   IOC-TYPE :  found | failed ")
+    stats_to_clear+=1
+
+    for key in stats:
+        if stats[key][0]+stats[key][1]>0:
+            stats_to_clear+=1
+            print("[++] {:^10} : {:>7d}|{:d}".format(key,stats[key][0],stats[key][1]))
+
+def close_files():
+    if not quiet : print("[+] Closing Files")
+    try:
+        for hashalgo in hashes: xways_files[hashalgo].close()
+        filenames_file.close()
+        def_file.close()
+    except:
+        pass
+
+    if not quiet : print("[+] Done!")
+
 hashes={
     "md5":{
         "length" : 32,
@@ -20,80 +69,78 @@ hashes={
         "text" : "SHA-384"},
     "sha512":{
         "length" : 128,
-        "text" : "SHA-512"}
+        "text" : "SHA-512"
+        }
     }
+
+#Variable declaration
+xways_files={}
+misp_export='misp.json'
+quiet=False
+interactive=False
 xways_files={}
 stats={}
 progress={}
 stats_to_clear=0
 stats_update_interval=10
+event_count=0
+attribute_count=0
+
+#Parsing Commandline Arguments
+try:
+    opts, args = getopt.getopt(sys.argv[1:],"hiqf:")
+except getopt.GetoptError: print_usage(2)
+for option, argument in opts:
+    if option == '-h': print_usage(0)
+    elif option == '-i':
+        interactive=True
+    elif option == '-q':
+        quiet=True
+    elif option in ("-f"): misp_export = argument
 
 
-class JsonProgress(object):
-    def __init__(self):
-        self.count = 0
+#reading input file
+if not quiet : print("[+] Reading Json-Data from %s ..." % misp_export)
+try:
+    with open(misp_export) as data_file:
+        if quiet:
+            data = json.load(data_file)
+        else:
+            data = json.load(data_file,object_hook=JsonProgress())
+            print("\r[+] Done")
+except Exception as e:
+    print("[-] JSON File could not be processed")
+    print(e)
+    print(type(e))
+    exit(3)
 
-    def __call__(self, obj):
-        self.count += 1
-        if (self.count % 100)==0:
-            sys.stdout.write("\r%8d" % self.count)
-        return obj
-
-def clear_lines():
-    sys.stdout.write('\x1b[1A\x1b[2K'*stats_to_clear)
-    sys.stdout.write('\x1b[0G')
-
-def print_stats():
-    global stats_to_clear
-    clear_lines()
-    width=40
-    header=[]
-    if progress["event"][1]>0:
-        progress_barlength=(progress["event"][0]*width)/progress["event"][1]
-        progress_percentage=(progress["event"][0]*100)/progress["event"][1]
-
-    print("[++] [{:_<{wdt}}] {:03d}%".format("+"*int(progress_barlength),int(progress_percentage),wdt=width))
-    print("[++]   IOC-TYPE :  found | failed ")
-    stats_to_clear=2
-
-    for key in stats:
-        if stats[key][0]+stats[key][1]>0:
-            stats_to_clear+=1
-            print("[++] {:^10} : {:>7d}|{:d}".format(key,stats[key][0],stats[key][1]))
+response = data["response"]
 
 
 filenames_regex=re.compile(r"[^|]+")
 filenames_file=open("misp_filenames.list",'w')
+def_file=open("misp_all_hashes_lut.csv",'w')
+def_file.write("hash;hashtype;category;attribute_value;event_info;event_id\n")
 stats["filenames"]=[0,0]
 for hashalgo in hashes:
     pattern=r"(^|[^a-fA-F0-9])([a-fA-F0-9]{" + str(hashes[hashalgo]["length"]) + r"})($|[^a-fA-F0-9])"
     hashes[hashalgo]["regex"]=re.compile(pattern)
     stats[hashalgo]=[0,0]
-    def_file=open("misp_all_hashes_lut.csv",'w')
-    def_file.write("hash;hashtype;category;attribute_value;event_info;event_id\n")
 
     xways_files[hashalgo]=open("misp_" + hashalgo + "_xways.hashlist",'w')
     xways_files[hashalgo].write(hashes[hashalgo]["text"]+"\n")
 
-print("[+] Reading Jason-Data from %s ..." % misp_export)
-with open(misp_export) as data_file:
-    data = json.load(data_file,object_hook=JsonProgress())
-    # data = json.load(data_file)
-    print("\r[+] Done") # \r in the next line erases the progress output
-response = data["response"]
-event_count=0
-attribute_count=0
-print("[+] Extracting:")
 
-progress["event"]=[0,len(response)]
+if not quiet : print("[+] Extracting:")
+
+progress["event"]=[1,len(response)]
 progress["attribs"]=[0,0]
 progress["ioc"]=0
 
-print_stats()
 
 for i in response:
     event = i["Event"]
-    if progress["event"][0]%stats_update_interval==0:
+    if not quiet and progress["event"][0]%stats_update_interval==0:
         print_stats()
     if "Attribute" in event:
         progress["attribs"]=[0,len(event["Attribute"])]
@@ -118,11 +165,9 @@ for i in response:
                 else:
                     stats["filenames"][1]+=1
     progress["event"][0]+=1
-print_stats()
-print("[+] Done! Extracted {:d} IOCs".format(progress["ioc"]))
-print("[+] Closing Files")
-for hashalgo in hashes:
-    xways_files[hashalgo].close()
-filenames_file.close()
-def_file.close()
-print("[+] Done!")
+if not quiet :
+    print_stats()
+    print("[+] Done! Extracted {:d} IOCs".format(progress["ioc"]))
+
+
+close_files()
